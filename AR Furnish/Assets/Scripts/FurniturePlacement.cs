811ee375d5b3fd0434558592e8c01707b779cd0f
Jaler Sekar Maji <jalerse@gmail.com>
2024-06-25 16:24:30 +0700

    Fix script error furniture placement
    Tested and works

--- AR Furnish/Assets/Scripts/FurniturePlacement.cs
+++ AR Furnish/Assets/Scripts/FurniturePlacement.cs
@@ -7,10 +7,11 @@ using UnityEngine.XR.ARFoundation;
 
 public class FurniturePlacement : MonoBehaviour
 {
-    [SerializeField] private GameObject SpawnableFurniture;
+    public GameObject SpawnableFurniture;
     [SerializeField] private XROrigin xrOrigin;
     [SerializeField] private ARRaycastManager raycastManager;
     [SerializeField] private ARPlaneManager planeManager;
+
 
     private List<ARRaycastHit> raycastHits = new List<ARRaycastHit>();
 
@@ -18,16 +19,13 @@ public class FurniturePlacement : MonoBehaviour
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                bool collision = raycastManager.Raycast(Input.GetTouch(0).position, raycastHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon);
+            bool collision = raycastManager.Raycast(Input.GetTouch(0).position, raycastHits,
+                UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon);
 
-                if (collision && isButtonPressed() == false)
-                {
-                    GameObject _object = Instantiate(SpawnableFurniture);
-                    _object.transform.position = raycastHits[0].pose.position;
-                    _object.transform.rotation = raycastHits[0].pose.rotation;
-                }
+            if (collision && isButtonPressed() == false)
+            {
+                GameObject _object = Instantiate(SpawnableFurniture, raycastHits[0].pose.position,
+                    raycastHits[0].pose.rotation);
 
                 foreach (var planes in planeManager.trackables)
                 {
