a1ea6243e877790bcef7bbd874fa2a993422735b
Jaler Sekar Maji <jalerse@gmail.com>
2024-06-25 13:52:00 +0700

    AR Furnish Updates

---
+++ AR Furnish/Assets/Scripts/FurniturePlacement.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class FurniturePlacement : MonoBehaviour
+{
+    [SerializeField] private GameObject SpawnableFurniture;
+    [SerializeField] private XROrigin xrOrigin;
+    [SerializeField] private ARRaycastManager raycastManager;
+    [SerializeField] private ARPlaneManager planeManager;
+
+    private List<ARRaycastHit> raycastHits = new List<ARRaycastHit>();
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                bool collision = raycastManager.Raycast(Input.GetTouch(0).position, raycastHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon);
+
+                if (collision && isButtonPressed() == false)
+                {
+                    GameObject _object = Instantiate(SpawnableFurniture);
+                    _object.transform.position = raycastHits[0].pose.position;
+                    _object.transform.rotation = raycastHits[0].pose.rotation;
+                }
+
+                foreach (var planes in planeManager.trackables)
+                {
+                    planes.gameObject.SetActive(false);
+                }
+
+                planeManager.enabled = false;
+            }
+        }
+    }
+
+    public bool isButtonPressed()
+    {
+        if (EventSystem.current.currentSelectedGameObject?.GetComponent<Button>() == null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void SwitchFurniture(GameObject furniture)
+    {
+        SpawnableFurniture = furniture;
+    }
+
+
+}
